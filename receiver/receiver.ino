d05c7cdd468ff4d95024e374b87d31f8eca3b83d
Isaac Tan <isaactan@pal-nat186-132-170.itap.purdue.edu>
2020-03-06 14:52:02 -0500

    parseInt

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -3,6 +3,9 @@
 
 RH_ASK driver;
 
+uint8_t defaultDeviceSequence[2] = {'A', 'A'};
+uint8_t sequence[2];
+
 int outputPin = 9;
 void setup()
 {
@@ -10,6 +13,30 @@ void setup()
     Serial.begin(9600);  // Debugging only
     if (!driver.init())
          Serial.println("init failed");
+}
+
+
+// this function check whether the buffer contains
+// the default deveice sequence number
+bool checkBuf(uint8_t * buf) {
+    if (sizeof(buf) <= 2 * sizeof(uint8_t)) {
+        return false;
+    }
+    return (buf[0] == defaultDeviceSequence[0] && buf[1] == defaultDeviceSequence);
+}
+
+int parseInt(uint8_t * buf) {
+    int frequency = 0;
+    if (checkBuf) {
+        if (buf[2] <= '9' && buf[2] >= '0') {
+            int index = 2;
+            while(buf[index] != '#') {
+                frequency = frequency * 10 + (buf[index] - '0');
+                index++;
+            }
+        }
+    }
+    return frequency;
 }
 
 void loop()
